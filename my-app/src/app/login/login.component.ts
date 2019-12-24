66b0ccfccf7e3eb2545f23e0fe29d809efc83f54
X-AROK <vanyanadeev@mail.ru>
2019-12-24 15:51:35 +0300

    Шаверма

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -6,19 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  login: string
-  password: string
-
+  login: string;
+  password: string;
+  entered: boolean;
   constructor() { }
 
   ngOnInit() {
-    this.login = "Likanica"
-    this.password ="Likanica123"
+    this.login = "Likanica";
+    this.password ="Likanica2";
+    this.entered =false;
   }
 MakeLogin() {
   console.log(this.login)
   console.log(this.password)
-  if (this.login == "Likanica" && this.password == "Likanica123")
-  console.log("You have entered succesfully")
+  if (this.login == "Likanica" && this.password == "Likanica2") { 
+  this.entered = true
+  console.log("Вы вошли")
 }
+else if (this.login !== "Likanica" && this.password !== "Likanica2") {
+  const pass = localStorage.getItem("password")
+  const log = localStorage.getItem("login")
+  if (pass == this.login && log == this.password) {
+    this.entered = true
+    console.log("Хорошо")
+   }
+  }
+ }
 }
